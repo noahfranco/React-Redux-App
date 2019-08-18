2142a6936178c2360892acea24d11725d8dd1c58
noahfranco <noahfra@gmail.com>
2019-08-18 16:50:56 -0400

    adding axios calls and .get APIs

--- redux-app/src/action/IndexAction.js
+++ redux-app/src/action/IndexAction.js
@@ -4,4 +4,6 @@ export const FETCH_API_DATA_START = "FETCH_API_DATA_START";
 export const FETCH_API_DATA_SUCCESS = "FETCH_API_DATA_SUCCESS"; 
 export const FETCH_API_DATA_FAILER = "FETCH_API_DATA_FAILER"; 
 
-
+export const getData = () => {
+    
+}
